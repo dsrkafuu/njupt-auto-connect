8d89d042b5df2bb3427f1ddd7b7347f81b216b04
dsrkafuu <dsrkafuu@outlook.com>
2021-10-06 14:22:10 +0800

    chore: update example

--- example.sh
+++ example.sh
@@ -1,5 +1,8 @@
-cat >/tmp/nac.sh <<EOF
+# write script to ram
+cat >/tmp/nac.sh <<"EOF"
 <COPY ALL CONTENTS HERE FROM THE SCRIPT>
 EOF
+# set permission
+chmod 777 /tmp/nac.sh
 # append username with nothing, @njxy or @cmcc
 nohup /tmp/nac.sh "<USERNAME>" "<PASSWORD>" >/tmp/nac.log &
